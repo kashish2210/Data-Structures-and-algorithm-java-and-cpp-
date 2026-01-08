088a8ab941dabe94feeedc49dd25c4ae0597e96f
kashish shrivastav <en.shrivastavkashish@gmail.com>
2026-01-08 13:54:38 +0530

    homework

---
+++ assignments/string.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class string {
+    public static void main(String args[]) {
+        // Scanner sc = new Scanner(System.in);
+        // String str = sc.nextLine();
+        String str = "ShardhaDidi";
+        String str1 = "ApnaCollege";
+        String str2 = "ShardhaDidi";
+        String str3 = str1.replace("A","");
+        System.out.println(str.equals(str1) + "  " + str.equals(str2));
+        System.out.println(str3);
+    }
+}
