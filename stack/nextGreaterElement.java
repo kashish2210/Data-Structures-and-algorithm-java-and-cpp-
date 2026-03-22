fd391b300d7b415d1bdc1ffd6995a8e39d0fe7d1
kashish shrivastav <en.shrivastavkashish@gmail.com>
2026-03-23 01:40:10 +0530

    learning stack

---
+++ stack/nextGreaterElement.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class nextGreaterElement{
+    public static void main(String args[]){
+        int[] arr = {6,8,0,1,3};
+        int[] nextGrt = new int[arr.length];
+
+        for(int i  = 0 ; i < nextGrt.length ; i++){
+            System.out.print(nextGrt[i] + " ");
+        }
+    }
+}
